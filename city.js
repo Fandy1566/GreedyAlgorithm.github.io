f3dec52fba73ad5fc26675ac39a2cf7fea6c177c
Fandy <100325135+Fandy1566@users.noreply.github.com>
2023-06-03 15:26:11 +0700

    Upload Website

---
+++ city.js
@@ -0,0 +1,53 @@
+class City {
+    constructor(name, distance, circlePos, weight = 0, profit = 0) {
+        this.name = name;
+        this.distance = distance;
+        this.circlePos = circlePos;
+        this.weight = weight;
+        this.profit = profit
+    }
+
+    getProfit(){
+        return this.profit;
+    }
+
+    setProfit(profit){
+        this.profit = profit
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    setName(name){
+        this.name = name
+    }
+
+    getDistance(n) {
+        return this.distance[n];
+    }
+
+    getDistances() {
+        return this.distance;
+    }
+
+    setDistances(distance){
+        this.distance = distance
+    }
+    
+    getCirclePosition() {
+        return this.circlePos;
+    }
+
+    setCirclePosition(circlePos){
+        this.circlePos = circlePos;
+    }
+
+    getWeight(){
+        return this.weight;
+    }
+
+    setWeight(weight){
+        this.weight = weight;
+    }
+}
